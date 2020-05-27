fc9ef4ba68d1e230c32f1668c8e6aa89fa7be197
Santhosh B. Appan <santzapaul1990@gmail.com>
2020-05-26 20:50:39 -0400

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
