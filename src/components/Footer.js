5b3089bd97b94fc1cb1b5a95fbf6b5c4b5e667ef
Santhosh B. Appan <santzapaul1990@gmail.com>
2020-05-26 20:45:53 -0400

    Added a Header, Footer & a basic Content component

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+  return <footer>© 2020. Gather Inc.</footer>;
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <h2>Gather</h2>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
