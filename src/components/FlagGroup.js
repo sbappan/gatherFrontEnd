6dac6883169d87a13d74818f9eb3ddceb2464532
Santhosh B. Appan <12586088+sbappan@users.noreply.github.com>
2020-06-10 11:15:27 -0400

    ViewAllGroups updated with FlagGroup and UnflagGroup options

--- src/components/FlagGroup.js
+++ src/components/FlagGroup.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import UnflagButton from './UnflagButton';
 
 class FlagGroup extends Component {
   constructor(props) {
@@ -6,7 +8,12 @@ class FlagGroup extends Component {
 
     this.state = {
       group: {},
+      reason: '',
+      isFlagged: false,
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async componentDidMount() {
@@ -14,7 +21,7 @@ class FlagGroup extends Component {
       const groupId = this.props.match.params._id;
       const res = await fetch(`${process.env.REACT_APP_API_LINK}/groups/${groupId}`);
       const group = await res.json();
-      this.setState({ group });
+      this.setState({ group, reason: group.status.reason, isFlagged: group.status.isFlagged });
     } catch (error) {
       console.log('error: ', error);
     }
@@ -25,16 +32,60 @@ class FlagGroup extends Component {
     this.setState({ [name]: value });
   }
 
-  handleSubmit(event) {
+  handleSubmit(event, groupId) {
     event.preventDefault();
-    console.log('Form submitted');
-    // const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length);
-    // const randMemeImg = this.state.allMemeImgs[randNum].url;
-    // this.setState({ randomImg: randMemeImg });
+
+    const { reason } = this.state;
+    const method = 'PUT';
+    const bodyData = {
+      status: {
+        isFlagged: true,
+        reason,
+      },
+    };
+    console.log('bodyData: ', bodyData);
+
+    fetch(`${process.env.REACT_APP_API_LINK}/groups/${groupId}`, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bodyData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ isFlagged: true });
+      })
+      .catch((error) => {
+      });
+  }
+
+  handleClick(groupId) {
+    const method = 'PUT';
+    const bodyData = {
+      status: {
+        isFlagged: false,
+        reason: '',
+      },
+    };
+
+    fetch(`${process.env.REACT_APP_API_LINK}/groups/${groupId}`, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bodyData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ isFlagged: false, reason: '' });
+      })
+      .catch((error) => {
+      });
   }
 
   render() {
-    const { group } = this.state;
+    const { group, reason, isFlagged } = this.state;
 
     return (
       <div className="flagGroup">
@@ -43,13 +94,32 @@ class FlagGroup extends Component {
         <p>{group.name}</p>
         <h3>Group Description:</h3>
         <p>{group.description}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>Reason:</p>
-          <textarea />
+        <h3>Group status:</h3>
+        <p>{isFlagged ? 'Flagged' : 'Not flagged'}</p>
+        <form onSubmit={(e) => this.handleSubmit(e, group._id)}>
+          <h3>Reason:</h3>
+          <textarea
+            name="reason"
+            placeholder="Please enter the reason for flagging the group"
+            value={reason}
+            onChange={this.handleChange}
+            required
+          />
           <br />
           <br />
           <button type="submit" className="danger">Submit</button>
-          <button type="button" className="success">Cancel</button>
+
+          { isFlagged
+            ? (
+              <>
+                <UnflagButton groupId={group._id} handleClick={this.handleClick} />
+                <Link to="/groups/">
+                  <button type="button" className="success">Back to groups list</button>
+                </Link>
+              </>
+            )
+            : <Link to="/groups/"><button type="button" className="safe">Cancel</button></Link>}
+
         </form>
       </div>
     );
