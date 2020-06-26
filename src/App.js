25a0828b12010dd3c3ba8a26b75cc3007537f950
Santhosh B. Appan <12586088+sbappan@users.noreply.github.com>
2020-06-26 16:24:21 -0400

    Updated App.js to match the master branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
